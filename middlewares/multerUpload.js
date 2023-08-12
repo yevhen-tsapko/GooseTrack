0bb9f16a705fcffebb25a054bad894d078ff0a34
Yevhen Tsapko <evgentsa@gmail.com>
2023-08-12 12:34:57 +0300

    del comments

--- middlewares/multerUpload.js
+++ middlewares/multerUpload.js
@@ -1,28 +1,4 @@
 const multer = require("multer");
-// const path = require("path");
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, path.join(__dirname, "..", "upload"));
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, "avatar-" + req.user.id); // file name - user id
-//   },
-//   fileFilter: (req, file, cb) => {
-//     if (file.mimetype === "image/png" || file.mimetype === "image/jpeg") {
-//       cb(null, true);
-//     } else {
-//       cb(null, false);
-//     }
-//   },
-//   limits: {
-//     fileSize: 1048576,
-//   },
-// });
-// const multerUpload = multer({ storage });
-
-// module.exports = multerUpload;
-
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
@@ -36,9 +12,9 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: async (req, file) => {
     return {
-      folder: "uploads",
-      allowed_formats: ["jpg", "png"], // Adjust the allowed formats as needed
-      public_id: "avatar-" + req.user.id, // Use original filename as the public ID
+      folder: "goose-track",
+      allowed_formats: ["jpg", "png"],
+      public_id: "avatar-" + req.user.id,
     };
   },
 });
@@ -46,4 +22,3 @@ const storage = new CloudinaryStorage({
 const upload = multer({ storage });
 
 module.exports = upload;
-
