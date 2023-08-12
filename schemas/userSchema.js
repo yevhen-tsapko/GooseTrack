909532a53a0f0eaaddf5700727ed23644b5f335f
Yevhen Tsapko <evgentsa@gmail.com>
2023-08-12 14:19:28 +0300

    Update userSchema.js

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 const userSchema = Joi.object({
   name: Joi.string(),
   email: Joi.string(),
-  phone: Joi.string(),
+  phone: Joi.string().pattern(/^\d{2} \(\d{3}\) \d{3} \d{2} \d{2}$/),
   skype: Joi.string(),
   bithday: Joi.string().pattern(/^\d{2}\/\d{2}\/\d{4}$/),
   theme: Joi.string(),
