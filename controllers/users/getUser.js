8d137bca66b72f49b22048e2f74cccd60ce222c5
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-08-12 16:29:29 +0300

    Improved endpoints and logic of current user

--- controllers/users/getUser.js
+++ controllers/users/getUser.js
@@ -1,9 +1,7 @@
 const User = require("../../models/users");
 const getUser = async (req, res) => {
   const user = await User.findById(req.user.id);
-  if (!user) {
-    return res.status(401).json({ message: "Not authorized" });
-  }
+
   return res.status(200).json(user);
 };
 module.exports = getUser;
