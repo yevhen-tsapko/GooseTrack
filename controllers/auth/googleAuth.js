87abd21118cacfdff46d4524349d7519a2136c7e
Yevhen Tsapko <evgentsa@gmail.com>
2023-08-15 18:02:13 +0300

    add redirect

--- controllers/auth/googleAuth.js
+++ controllers/auth/googleAuth.js
@@ -1,5 +1,11 @@
+const FRONTEND_URL = process.env;
 const createSessionAndTokens = require("../../helpers/createNewSessionAndTokens");
 const googleAuth = async (req, res) => {
-  const tokens = await createSessionAndTokens(user.id);
-  res.redirect(``);
+  const { accessToken, refreshToken } = await createSessionAndTokens(
+    req.user._id
+  );
+  res.redirect(
+    `${FRONTEND_URL}/api/auth/login?accessToken=${accessToken}&refreshToken=${refreshToken} }`
+  );
 };
+module.export = googleAuth;
