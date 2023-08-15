ea4ac87eeee55dfa4fb6dc07723a4c9156add408
Yevhen Tsapko <evgentsa@gmail.com>
2023-08-15 08:07:47 +0300

    fix addTask route

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -1,10 +1,7 @@
 const express = require("express");
 
 const { auth, isValidTaskId, validateBody } = require("../../middlewares");
-
-
-
-
+const taskSchema = require("../../schemas/taskSchema.js");
 
 const router = express.Router();
 
@@ -12,12 +9,10 @@ const ctrl = require("../../controllers/tasks");
 
 router.get("/", auth, ctrl.getAll);
 
-
 router.post("/", auth, validateBody(taskSchema), ctrl.addTasks);
 
 router.delete("/:taskId", auth, isValidTaskId, ctrl.removeById);
 
 router.patch("/:taskId", auth, isValidTaskId, ctrl.update);
-
 
 module.exports = router;
