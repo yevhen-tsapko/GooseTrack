00ee37c9b7343669f449336e7c23930a37bbe59c
Bivis60 <89808390+Bivis60@users.noreply.github.com>
2023-08-15 19:41:24 +0300

    Закінчив PATCH/users/current

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -2,9 +2,7 @@ const express = require("express");
 
 const { auth, isValidTaskId, validateBody } = require("../../middlewares");
 
-
-
-
+const taskSchema = require("../../schemas");
 
 const router = express.Router();
 
@@ -12,12 +10,10 @@ const ctrl = require("../../controllers/tasks");
 
 router.get("/", auth, ctrl.getAll);
 
-
 router.post("/", auth, validateBody(taskSchema), ctrl.addTasks);
 
 router.delete("/:taskId", auth, isValidTaskId, ctrl.removeById);
 
 router.patch("/:taskId", auth, isValidTaskId, ctrl.update);
-
 
 module.exports = router;
