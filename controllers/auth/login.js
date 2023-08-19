39df0efef7d16a7b04b5315d998f811a499997a5
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-08-19 17:29:24 +0300

    Added checking if user is verified in login

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,24 +1,36 @@
 const User = require("../../models/users");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+
 const login = async (req, res) => {
   const { email, password } = req.body;
+
   const user = await User.findOne({ email });
+
   if (user === null) {
     return res.status(401).json({ message: "Email or password is wrong" });
   }
+
+  if (!user.verify) {
+    return res.status(401).json({ message: "Email not verified" });
+  }
+
   const isMatch = await bcrypt.compare(password, user.password);
+
   if (isMatch) {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "23h",
     });
+
     const updatedUser = await User.findByIdAndUpdate(
       user._id,
       { token },
       { new: true }
     );
+
     return res.status(200).json(updatedUser);
   }
+
   return res.status(401).json({ message: "Email or password is wrong" });
 };
 
