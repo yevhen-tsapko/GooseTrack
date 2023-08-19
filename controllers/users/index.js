a7c6f116f4a8fcb9d4f126cd7ce597f125e777e1
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-08-19 18:20:43 +0300

    Added updating password controller

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -1,10 +1,12 @@
 const updateUser = require("../users/updateUser");
 const getUser = require("../users/getUser");
 const deleteUser = require("../users/deleteUser");
+const updatePassword = require("../users/updatePassword");
 const contrsWrapper = require("../../helpers/contrsWrapper");
 
 module.exports = {
   getUser: contrsWrapper(getUser),
   updateUser: contrsWrapper(updateUser),
   deleteUser: contrsWrapper(deleteUser),
+  updatePassword: contrsWrapper(updatePassword),
 };
