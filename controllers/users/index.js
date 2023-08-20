9951f38c058794056a9d4977efb140babaed6c78
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-08-20 15:57:32 +0300

    Added updating new password controller

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -2,6 +2,7 @@ const updateUser = require("../users/updateUser");
 const getUser = require("../users/getUser");
 const deleteUser = require("../users/deleteUser");
 const updatePassword = require("../users/updatePassword");
+const updateForgottenPassword = require("../users/updateForgottenPassword");
 const verify = require("../users/verify");
 const resendVerify = require("../users/resendVerify");
 const contrsWrapper = require("../../helpers/contrsWrapper");
@@ -11,6 +12,7 @@ module.exports = {
   updateUser: contrsWrapper(updateUser),
   deleteUser: contrsWrapper(deleteUser),
   updatePassword: contrsWrapper(updatePassword),
+  updateForgottenPassword: contrsWrapper(updateForgottenPassword),
   verify: contrsWrapper(verify),
   resendVerify: contrsWrapper(resendVerify),
 };
