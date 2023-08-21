5f09376628960f862c323e78b9d6a72cb6f0b0de
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-08-21 23:47:05 +0300

    Added avatar to review

--- controllers/reviews/createReview.js
+++ controllers/reviews/createReview.js
@@ -1,6 +1,20 @@
 const Review = require("../../models/reviews");
+const User = require("../../models/users");
 const createReview = async (req, res) => {
-  const result = await Review.create({ ...req.body, ownerId: req.user.id });
+  const { id } = req.user;
+
+  const user = await User.findById(id);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  const { avatarURL } = user;
+
+  const result = await Review.create({
+    ...req.body,
+    avatar: avatarURL,
+    ownerId: id,
+  });
   res.status(201).json(result);
 };
 
