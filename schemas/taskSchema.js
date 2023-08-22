2551996b67baab490eabc06e2e19158d6ef5cde8
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-08-22 17:18:44 +0300

    Fixed validation

--- schemas/taskSchema.js
+++ schemas/taskSchema.js
@@ -1,31 +1,46 @@
 const Joi = require("joi");
+const moment = require("moment");
 
 const taskSchema = Joi.object({
   title: Joi.string().max(250).required(),
-  start: Joi.string().required(),
-  end: Joi.string().required(),
+  start: Joi.string()
+    .custom((value, helpers) => {
+      const startTime = moment(value, "HH:mm", true);
+      if (!startTime.isValid()) {
+        return helpers.error("any.invalid");
+      }
+      return value;
+    })
+    .required(),
+  end: Joi.string()
+    .custom((value, helpers) => {
+      const endTime = moment(value, "HH:mm", true);
+      if (!endTime.isValid()) {
+        return helpers.error("any.invalid");
+      }
+      return value;
+    })
+    .required(),
   priority: Joi.string()
     .valid("low", "medium", "high")
     .default("low")
     .required(),
-  date: Joi.date().iso().error(new Error(" Is not valid date format")).required(),
+  date: Joi.date()
+    .iso()
+    .error(new Error("Is not a valid date format"))
+    .required(),
   category: Joi.string().valid("to-do", "in-progress", "done").required(),
 }).custom((value, helpers) => {
-  const { start, end } = value;
+  const startTime = moment(value.start, "HH:mm");
+  const endTime = moment(value.end, "HH:mm");
 
-  const startTime = parseTime(start);
-  const endTime = parseTime(end);
-
-  if (startTime > endTime) {
-    return helpers.error("End time must be greater than start time");
+  if (startTime.isAfter(endTime)) {
+    return helpers.error("any.invalid", {
+      message: "Start time must be before end time",
+    });
   }
 
   return value;
-});
-
-function parseTime(timeString) {
-  const [hours, minutes] = timeString.split(":").map(Number);
-  return hours * 60 + minutes;
-}
+}, "custom validation");
 
 module.exports = taskSchema;
