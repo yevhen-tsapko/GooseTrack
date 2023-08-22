60e2193bfc8f5ccfa0c96ba26c8fc569f8f02192
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-08-22 17:47:35 +0300

    Revert "Fixed validation"
    This reverts commit 2551996b67baab490eabc06e2e19158d6ef5cde8.

--- schemas/taskSchema.js
+++ schemas/taskSchema.js
@@ -1,46 +1,31 @@
 const Joi = require("joi");
-const moment = require("moment");
 
 const taskSchema = Joi.object({
   title: Joi.string().max(250).required(),
-  start: Joi.string()
-    .custom((value, helpers) => {
-      const startTime = moment(value, "HH:mm", true);
-      if (!startTime.isValid()) {
-        return helpers.error("any.invalid");
-      }
-      return value;
-    })
-    .required(),
-  end: Joi.string()
-    .custom((value, helpers) => {
-      const endTime = moment(value, "HH:mm", true);
-      if (!endTime.isValid()) {
-        return helpers.error("any.invalid");
-      }
-      return value;
-    })
-    .required(),
+  start: Joi.string().required(),
+  end: Joi.string().required(),
   priority: Joi.string()
     .valid("low", "medium", "high")
     .default("low")
     .required(),
-  date: Joi.date()
-    .iso()
-    .error(new Error("Is not a valid date format"))
-    .required(),
+  date: Joi.date().iso().error(new Error(" Is not valid date format")).required(),
   category: Joi.string().valid("to-do", "in-progress", "done").required(),
 }).custom((value, helpers) => {
-  const startTime = moment(value.start, "HH:mm");
-  const endTime = moment(value.end, "HH:mm");
+  const { start, end } = value;
 
-  if (startTime.isAfter(endTime)) {
-    return helpers.error("any.invalid", {
-      message: "Start time must be before end time",
-    });
+  const startTime = parseTime(start);
+  const endTime = parseTime(end);
+
+  if (startTime > endTime) {
+    return helpers.error("End time must be greater than start time");
   }
 
   return value;
-}, "custom validation");
+});
+
+function parseTime(timeString) {
+  const [hours, minutes] = timeString.split(":").map(Number);
+  return hours * 60 + minutes;
+}
 
 module.exports = taskSchema;
